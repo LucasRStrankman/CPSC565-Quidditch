e961abaa95bd8a3f075d9c19ab8d7fb001e05890
lucasrstrankman <lucas.ramosstrankman@ucalgary.ca>
2020-03-08 10:53:36 -0600

    snitch
    Started trying to make the snitch

--- CPSC565-Quidditch/Assets/AwayWall.cs
+++ CPSC565-Quidditch/Assets/AwayWall.cs
@@ -3,29 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-public class Forward : MonoBehaviour {
-	public float thrust;
+public class AwayWall : MonoBehaviour {
 	public GameObject[] walls;
 	Rigidbody body;
 	Vector3 m_thrust;
-	Vector3 wall1;
-	Vector3 wall2;
-	Vector3 wall3;
-	Vector3 wall4;
+	BoxCollider collider;
+	public float thrust;
 
 	// Use this for initialization
 	void Start () {
 		body = GetComponent<Rigidbody> ();
+		collider = walls[0].GetComponent<BoxCollider> ();
 		m_thrust = new Vector3 (0, 0, thrust);
 	}
-
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
 	void FixedUpdate () {
-	//	wall1 = body.transform.position - walls [0].transform.position;
-	//	float distance = Vector3.Distance (body.transform.position, walls [0].transform.position);
-	//	wall1 = wall1 / distance;
-	//	wall1 = wall1 * 10;
-
+		Vector3 point = collider.ClosestPointOnBounds (body.transform.position);
 		body.AddForce (m_thrust);
-	//	body.AddForce (wall1);
+		Debug.Log (point);
+
+
 	}
 }
+
+	
